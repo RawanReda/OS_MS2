cf5fb634ddf0c41ac3229bff84f12c513f93e7a5
Rowan Amgad <rowan.amgad4@gmail.com>
2021-06-23 15:59:19 +0200

    allocator

--- OS/src/CPU.java
+++ OS/src/CPU.java
@@ -1,8 +1,64 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 public class CPU {
+
+    private Word[] memory;
+    private int pidcount;
+    private int memsize;
+
 
     // memory
     // round robin
     // pcb
 
+    public CPU() {
+        memory = new Word[32];
+        pidcount = 1;
+        memsize = 0;
+    }
 
+    public void allocator(File file) throws IOException {
+        PCB();
+        int ins = 0;
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null) {
+            ins++;
+            memory[memsize] = new Word("Ins" + (ins + ""), st);
+            memsize++;
+        }
+        memsize+=2;
+        memory[(memsize-ins-3)].setValue(memory[(memsize-ins-3)].getValue() + (memsize + "")); //process end boundary concatenation
+        pidcount++;
+    }
+
+    public void PCB ()
+    {
+        int temp = memsize;
+        memory[memsize] = new Word("Process Id", pidcount +""); // process id
+        memsize ++;
+        memory[memsize] = new Word("Process State", "Not Running"); // process state
+        memsize ++;
+        memory[memsize] = new Word("PC", (memsize + 2) +""); // PC
+        memsize ++;
+        memory[memsize] = new Word("Memory Boundaries", temp+ "" + ","); // memory boundaries
+        memsize ++;
+    }
+
+    public static void main(String[] args) throws IOException {
+        CPU C = new CPU();
+        File file1 = new File("Program 1.txt");
+        C.allocator(file1);
+        File file2 = new File("Program 2.txt");
+        C.allocator(file2);
+        File file3 = new File("Program 3.txt");
+        C.allocator(file3);
+
+//writeFile("Test","1");
+    }
 }
+
+
---
+++ OS/src/Word.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Word {
+
+    private String key;
+    private String value;
+
+    public Word(String key, String value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
